d183f0933c0a5a4fca75c981757cc161f5e0846f
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-20 16:37:01 +0000

    Add pall function

--- pall.c
+++ pall.c
@@ -1,6 +1,20 @@
 #include "monty.h"
 
+
+/**
+ * pall - print out Stack/Queue data
+ * @stack: given stack/queue
+ * @line_number: given command line number
+ *
+ * Return: Nothing
+*/
 void pall(stack_t **stack, unsigned int line_number)
 {
-	return;
+	stack_t *temp = *stack;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
 }
