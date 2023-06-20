20f28d30acf7a82d005acc3d438dc92e41882b59
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-20 15:58:46 +0000

    update excute_op file
    added added a check for each stack function

--- execute_op.c
+++ execute_op.c
@@ -1,18 +1,21 @@
 #include "monty.h"
 
 
-void execute_op(char **command)
+void execute_op(char **command, unsigned int line_number)
 {
+	stack_t *top = NULL;
 	instruction_t op_list[] = {
-		{"push", push},
 		{"pall", pall},
 		{NULL, NULL}
 	};
 	int i;
 
+	if (strcmp(command[0], "push") == 0)
+		push(&top, line_number, command[1]);
+
 	for (i = 0; op_list[i].opcode; i++)
 	{
 		if (strcmp(command[0], op_list[i].opcode) == 0)
-			printf("execute: %s\n", op_list[i].opcode);
+			op_list[i].f(&top, line_number);
 	}
 }
