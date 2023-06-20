1a8125c4e1eeefc2a1993a0ccb0a997461b23c9e
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-20 16:31:31 +0000

    Update execute_op add decription, add stack parameter
    added stack parameter to keep track of stack changes

--- execute_op.c
+++ execute_op.c
@@ -1,9 +1,15 @@
 #include "monty.h"
 
-
-void execute_op(char **command, unsigned int line_number)
+/**
+ * execute_op - execute a given command
+ * @top: given stack/queue
+ * @command: given command
+ * @line_number: given command line number
+ *
+ * Return: Nothing
+*/
+void execute_op(stack_t **top, char **command, unsigned int line_number)
 {
-	stack_t *top = NULL;
 	instruction_t op_list[] = {
 		{"pall", pall},
 		{NULL, NULL}
@@ -11,11 +17,11 @@ void execute_op(char **command, unsigned int line_number)
 	int i;
 
 	if (strcmp(command[0], "push") == 0)
-		push(&top, line_number, command[1]);
+		push(&(*top), line_number, command[1]);
 
 	for (i = 0; op_list[i].opcode; i++)
 	{
 		if (strcmp(command[0], op_list[i].opcode) == 0)
-			op_list[i].f(&top, line_number);
+			op_list[i].f(&(*top), line_number);
 	}
 }
