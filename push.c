e0ed01e23d444998393a6cfedb8098b020573b94
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-20 16:01:11 +0000

    added push function

--- push.c
+++ push.c
@@ -1,6 +1,31 @@
 #include "monty.h"
 
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number, char *data)
 {
-	return;
+	stack_t *new_stack;
+	int data_num;
+
+	new_stack = malloc(sizeof(stack_t));
+	if (!new_stack)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	data_num = atoi(data);
+	if (!data || data_num == 0)
+	{
+		fprintf(stderr, "%d: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_stack->n = data_num;
+	new_stack->prev = NULL;
+
+	if (!stack)
+		new_stack->next = NULL;
+	
+	else
+		new_stack->next = *stack;
+	
+	*stack = new_stack;
 }
