7c9add55447879aa0d2904d53087cc708c645f2c
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-21 05:46:25 +0000

    Update push add free for lines, stack
    free lines(given file lines), stack(top) after error

--- push.c
+++ push.c
@@ -4,12 +4,13 @@
 /**
  * push - add node to the Stack/Queue
  * @stack: given stack/queue
+ * @lines: given file lines to free
  * @line_number: given command line number
  * @data: given node data
  *
  * Return: Nothing
 */
-void push(stack_t **stack, unsigned int line_number, char *data)
+void push(stack_t **stack, char **lines, unsigned int line_number, char *data)
 {
 	stack_t *new_stack;
 	int data_num;
@@ -17,12 +18,16 @@ void push(stack_t **stack, unsigned int line_number, char *data)
 	new_stack = malloc(sizeof(stack_t));
 	if (!new_stack)
 	{
+		free_tokens(lines);
+		free_dlist(*stack);
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	data_num = atoi(data);
 	if (strcmp(data, "nil") == 0 || data_num == 0)
 	{
+		free_tokens(lines);
+		free_dlist(*stack);
 		fprintf(stderr, "%d: usage: push integer", line_number);
 		exit(EXIT_FAILURE);
 	}
