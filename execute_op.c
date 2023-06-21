a11cc90581a15b32b46256c3b0acac8ebc6d15ff
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-21 05:49:13 +0000

    Update execute add lines to push
    added lines to push opcode to free it out after error

--- execute_op.c
+++ execute_op.c
@@ -8,7 +8,7 @@
  *
  * Return: Nothing
 */
-void execute_op(stack_t **top, char **command, unsigned int line_number)
+void execute_op(stack_t **top, char **lines, char **command, unsigned int l_num)
 {
 	instruction_t op_list[] = {
 		{"pall", pall},
@@ -18,7 +18,7 @@ void execute_op(stack_t **top, char **command, unsigned int line_number)
 
 	if (strcmp(command[0], "push") == 0)
 	{
-		push(&(*top), line_number, command[1]);
+		push(&(*top), lines, l_num, command[1]);
 		return;
 	}
 
@@ -26,11 +26,11 @@ void execute_op(stack_t **top, char **command, unsigned int line_number)
 	{
 		if (strcmp(command[0], op_list[i].opcode) == 0)
 		{
-			op_list[i].f(&(*top), line_number);
+			op_list[i].f(&(*top), l_num);
 			return;
 		}
 	}
 
-		fprintf(stderr, "L%d: unknown instruction %s", line_number, command[0]);
+		fprintf(stderr, "L%d: unknown instruction %s", l_num, command[0]);
 		exit(EXIT_FAILURE);
 }
