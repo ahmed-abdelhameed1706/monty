82ef9d45a383db4d5c0bd7aebe1ac95288922979
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-06-22 10:53:30 +0000

    Fix typo

--- pchar.c
+++ pchar.c
@@ -12,7 +12,7 @@ void pchar(stack_t **stack, unsigned int line_num)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_num);
 		free_tokens(gv.tokens);
 		free_tokens(gv.lines);
 		free_dlistint(*stack);
