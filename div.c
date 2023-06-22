22448351236c1afefe5c8f77b3c20c76623bf433
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-22 02:12:04 -0700

    adding div

---
+++ div.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * _div - div 2 n from stack
+ * @stack: stack to be added from
+ * @line_num : line number
+ * 
+ * Return: Nothing 
+ */
+void _div(stack_t **stack, unsigned int line_num)
+{
+	stack_t *temp;
+	int div;
+
+	temp = *stack;
+
+	if (temp == NULL || temp->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short", line_num);
+		free_dlistint(*stack);
+		free_tokens(gv.tokens);
+		free_tokens(gv.lines);
+		exit(EXIT_FAILURE);
+	}
+
+	div = temp->next->n / temp->n;
+	temp->next->n = div;
+	*stack = temp->next;
+	free(temp);
+}
