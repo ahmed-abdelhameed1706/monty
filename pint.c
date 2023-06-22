21ecfe264758b1bf45bbc6173fd5168fd7b4fc6b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-22 01:23:56 -0700

    pint.c

--- pint.c
+++ pint.c
@@ -4,7 +4,7 @@ void pint(stack_t **stack, unsigned int line_num)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		free_tokens(gv.tokens);
 		free_tokens(gv.lines);
 		free_dlistint(*stack);
